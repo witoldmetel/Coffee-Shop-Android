585b24b98ebd6dbf91b76fbc03c17f2ba6d25942
witoldmetel <witoldmetel@gmail.com>
2023-06-02 19:00:01 +0200

    add list

--- CoffeeShop/app/src/main/java/witoldmetel/coffeeshop/pages/MenuPage.kt
+++ CoffeeShop/app/src/main/java/witoldmetel/coffeeshop/pages/MenuPage.kt
@@ -1,11 +1,92 @@
 package witoldmetel.coffeeshop.pages
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import witoldmetel.coffeeshop.Product
+import witoldmetel.coffeeshop.R
+import witoldmetel.coffeeshop.ui.theme.Alternative1
+import witoldmetel.coffeeshop.ui.theme.CardBackground
 
 @Preview(showBackground = true, widthDp = 400)
 @Composable
 fun MenuPage() {
-    Text("Menu Page")
+    LazyColumn {
+        items(5) {
+            Card(
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 2.dp
+                ),
+                shape = RoundedCornerShape(12.dp),
+                modifier = Modifier
+                    .background(CardBackground)
+                    .padding(12.dp)
+            ) {
+                ProductItem(product = Product(1,"Dummy", 1.22, ""), onAdd = {})
+            }
+        }
+    }
+}
+
+fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
+@Composable
+fun ProductItem(product: Product, onAdd: (Product)->Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.black_coffee),
+            contentDescription = "Image for ${product.name}",
+            contentScale = ContentScale.FillWidth,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Column {
+                Text(product.name, fontWeight = FontWeight.Bold)
+                Text("$${product.price.format(2)} ea")
+            }
+            Button(
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Alternative1,
+                    contentColor = Color.White
+                ),
+                onClick = {
+                    onAdd(product)
+                },
+            ) {
+                Text("Add")
+            }
+        }
+    }
 }
