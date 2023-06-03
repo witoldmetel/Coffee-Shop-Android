2840712f285448dc41374c432bd900cbf6287b79
witoldmetel <witoldmetel@gmail.com>
2023-06-03 12:12:39 +0200

    add web view

--- CoffeeShop/app/src/main/java/witoldmetel/coffeeshop/pages/InfoPage.kt
+++ CoffeeShop/app/src/main/java/witoldmetel/coffeeshop/pages/InfoPage.kt
@@ -1,12 +1,33 @@
 package witoldmetel.coffeeshop.pages
 
-import androidx.compose.material3.Text
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 
 
 @Preview(showBackground = true, widthDp = 400)
 @Composable
 fun InfoPage() {
-    Text("Info page")
+    MyWebView()
+}
+
+@Composable
+fun MyWebView() {
+    var url = "https://firtman.github.io/coffeemasters/webapp"
+
+    AndroidView(factory = {
+        WebView(it).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+            )
+            webViewClient = WebViewClient() // customize the web view
+            loadUrl(url)
+        }
+    }, update = {
+        it.loadUrl(url)
+    })
 }
